8b9876adde7d8984c2c8b29d48a43f262078b43f
katrine-mo-jensen <katrine.mo.jensen@gmail.com>
2023-05-30 13:35:55 +0200

    poj

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-import http from "http";
+import express from "express";
 
-http
-  .createServer((request, response) => {
-    response.writeHead(200, { "Content-Type": "text/html" });
-    response.write("Hello World! fuck");
-    response.end();
-  })
-  .listen(4000);
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hej verden!");
+});
+
+app.get("/about", (req, res) => {
+  res.send("Læs mere om min Node.js app!");
+});
+
+app.use((req, res, next) => {
+  res.status(404).send("Siden blev ikke fundet");
+});
+
+app.listen(4242, () => {
+  console.log("Server kører på http://localhost:4242/");
+});
---
+++ index_http.js
@@ -0,0 +1,11 @@
+import http from "http";
+
+http
+  .createServer((request, response) => {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.write("Hello World! fucker wup");
+    response.end();
+  })
+  .listen(4000, () => {
+    console.log("Node Server kører på http://localhost:4000/");
+  });
