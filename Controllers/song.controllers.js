ca5d9384388e45da01f1a184fa27a92b920e24f7
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-06-12 10:04:09 +0200

    update

--- Controllers/song.controllers.js
+++ Controllers/song.controllers.js
@@ -4,93 +4,94 @@ class SongController {
   constructor() {
     console.log("Class SongController instantiated");
   }
-  // list
-  /* 	list = () => {
-		console.log('Kører list metode');
-		return true
-	}
- */
-	list = (req, res) => {
-		// console.log(req.query);
-		// let { sortkey } = req.query
 
-		//sætter ternary operator
+  list = (req, res) => {
+    console.log(req.query);
+    let { sortkey, sortdir, limit, attributes } = req.query;
 
-		// sortkey = sortkey ? sortkey :'id' 
+    sortkey = sortkey ? sortkey : "s.id";
+	sortdir = sortdir ? sortdir.toUpperCase() : 'ASC'
+	limit = limit ? `LIMIT ${parseInt(limit)}` : ''
+	attributes = attributes ? attributes : 's.id, s.title, a.name'
 
-		const sql = `SELECT s.id, s.title, a.name, a.id AS artist_id
+    const sql = `SELECT ${attributes}
 						FROM song s 
 						JOIN artist a 
 						ON s.artist_id = a.id 
-						ORDER BY a.name`
-		db.query(sql, (err, result) => {
-			if(err) {
-				console.error(err)
-			} else {
-				res.json(result)
-			}
-		})
-	}
+						ORDER BY ${sortkey} ${sortdir} ${limit} `;
+						console.log(sql)
+    db.query(sql, (err, result) => {
+      if (err) {
+        console.error(err);
+      } else {
+        res.json(result);
+      }
+    });
+  };
 
-  // details
-/*   details = () => {
-    console.log("Kører details metode");
-    return true;
-  }; */
-
-	details = (req, res) => {
-		const id = parseInt(req.params.id)
-		const sql = `SELECT s.id, s.title, s.content, a.name 
+  details = (req, res) => {
+    const id = parseInt(req.params.id);
+    const sql = `SELECT s.id, s.title, s.content, a.name 
 						FROM song s 
 						JOIN artist a 
 						ON s.artist_id = a.id 
 						WHERE s.id = ? 
-						ORDER BY a.name`
-		db.query(sql, [id], (err, result) => {
-			if(err) {
-				console.error(err)
-			} else {
-				res.json(result)
-			}
-		})
-	}
+						ORDER BY a.name`;
+    db.query(sql, [id], (err, result) => {
+      if (err) {
+        console.error(err);
+      } else {
+        res.json(result);
+      }
+    });
+  };
 
-  // create
-/*   create = () => {
-    console.log("Kører create metode");
-    return true;
-  }; */
+  create = (req, res) => {
+    const { title, content, artist_id } = req.body;
+    //console.log({title, content, artist_id});
 
-	create = (req, res) => {
-		const { title, content, artist_id } = req.body
-		//console.log({title, content, artist_id});
-
-		if(title && content && artist_id) {
-			const sql = `
+    if (title && content && artist_id) {
+      const sql = `
 				INSERT INTO song(title, content, artist_id) 
 				VALUES(?,?,?)				
-			`
-			db.query(sql, [title, content, artist_id], (err, result) => {
-				if(err) {
-					console.error(err)
-				} else {
-					res.json({new_id: result.insertId})
-				}
-			})
-		}
+			`;
+      db.query(sql, [title, content, artist_id], (err, result) => {
+        if (err) {
+          console.error(err);
+        } else {
+          res.json({ new_id: result.insertId });
+        }
+      });
+    }
+  };
 
-	}
-  // update
-  update = () => {
-    console.log("Kører update metode");
-    return true;
+  update = (req, res) => {
+    const { title, content, artist_id, id } = req.body;
+    console.log(req.body);
+
+    if (title && content && artist_id && id) {
+      const sql = `
+				UPDATE song 
+				SET title = ?,
+				content = ?,
+				artist_id = ? 
+				WHERE id = ?
+			`;
+      db.query(sql, [title, content, artist_id, id], (err, result) => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log(sql);
+          res.json({
+            status: "ok",
+            updated_id: id,
+          });
+        }
+      });
+    }
   };
 
   // delete
-  delete = () => {
-    console.log("Kører delete metode");
-    return true;
-  };
 }
 
 export default SongController;
