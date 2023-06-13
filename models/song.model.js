e70b91dd081869a7ecde574635602aa754983d60
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-06-13 10:39:41 +0200

    Update song.model.js

--- models/song.model.js
+++ models/song.model.js
@@ -3,4 +3,29 @@ import { Sequelize, DataTypes, Model  } from "sequelize";
 
 class SongModel extends Model {}
 
-SongModel.init
+SongModel.init({
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: 'untitled'
+    },
+    content: {
+        type: DataTypes.TEXT,
+        allowNull: true
+    },
+    artist_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    sequelize,
+    modelName: 'song'
+
+})
+
+export default SongModel;
